c2421594766ecdb8eb851b090e332ed30e420cf0
mariacandelasoria2003-eng <mariacandelasoria2003@gmail.com>
2026-04-07 21:42:18 -0300

    Delete html/JS/script.js

--- html/JS/script.js
+++
@@ -1,6 +0,0 @@
-let boton = document.getElementById("menu-btn");
-let menu = document.getElementById("menu");
-
-boton.addEventListener("click", function() {
-    menu.classList.toggle("active");
-});
