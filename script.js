6c7842e380a0531082ea5f8a7f4789dd8433acef
mariacandelasoria2003-eng <mariacandelasoria2003@gmail.com>
2026-04-07 21:43:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let boton = document.getElementById("menu-btn");
+let menu = document.getElementById("menu");
+
+boton.addEventListener("click", function() {
+    menu.classList.toggle("active");
+});
